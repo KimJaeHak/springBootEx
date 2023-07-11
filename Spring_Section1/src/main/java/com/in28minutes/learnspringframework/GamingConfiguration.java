9706b588fcb387984d59d9a73beb533de52525ef
jh-kim21 <enginhak@naver.com>
2023-07-12 00:06:17 +0900

    위치 이동

--- Spring_Section1/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
+++ Spring_Section1/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -2,11 +2,8 @@ package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PackmanGame;
 
 @Configuration
